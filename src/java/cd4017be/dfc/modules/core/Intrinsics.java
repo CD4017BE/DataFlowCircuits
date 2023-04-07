de72299036b135ebc4ab5188bccb3d62dfac4c25
CD4017BE <cd4017be@gmail.com>
2023-04-07 16:48:22 +0200

    add File IO

--- src/java/cd4017be/dfc/modules/core/Intrinsics.java
+++ src/java/cd4017be/dfc/modules/core/Intrinsics.java
@@ -4,17 +4,19 @@ import static cd4017be.dfc.lang.Value.NO_DATA;
 import static cd4017be.dfc.lang.Value.NO_ELEM;
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardOpenOption.*;
+
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.channels.*;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.*;
+import java.util.*;
+
+import cd4017be.dfc.lang.*;
 import cd4017be.dfc.lang.Module;
-import cd4017be.dfc.lang.NodeContext;
-import cd4017be.dfc.lang.SignalError;
-import cd4017be.dfc.lang.Type;
-import cd4017be.dfc.lang.Value;
 import cd4017be.dfc.lang.instructions.IntrinsicLoader.Impl;
 import cd4017be.dfc.lang.instructions.IntrinsicLoader.Init;
 
@@ -22,7 +24,7 @@ import cd4017be.dfc.lang.instructions.IntrinsicLoader.Init;
  * @author cd4017be */
 public class Intrinsics {
 
-	public static Type VOID, INT, FLOAT, STRING;
+	public static Type VOID, INT, FLOAT, STRING, FILE;
 	public static Value NULL;
 
 	@Init
@@ -31,8 +33,90 @@ public class Intrinsics {
 		INT = m.types.get("int");
 		FLOAT = m.types.get("float");
 		STRING = m.types.get("string");
+		FILE = m.types.get("file");
 		NULL = new Value(VOID, NO_ELEM, NO_DATA, 0);
 	}
+
+	//File operations:
+
+	@SuppressWarnings("unchecked")
+	private static final Set<OpenOption>[] FILE_MODES = new Set[] {
+		Set.of(READ),
+		Set.of(WRITE, CREATE, TRUNCATE_EXISTING),
+	};
+
+	@Impl(inputs = 2, useIp = true)
+	public static Value fileOpen(Interpreter ip, byte[] path, long mode) {
+		if (mode < 0 || mode >= FILE_MODES.length)
+			throw new IllegalArgumentException("invalid open mode");
+		try {
+			return new Value(FILE, NO_ELEM, NO_DATA, ip.addResource(
+				Files.newByteChannel(Path.of(new String(path, UTF_8)), FILE_MODES[(int)mode])
+			));
+		} catch(IOException e) {
+			return new Value(FILE, NO_ELEM, e.toString().getBytes(UTF_8), -1);
+		}
+	}
+
+	@Impl(inputs = 4, useIp = true)
+	public static Value fileRead(Interpreter ip, Value file, byte[] data, long ofs, long len) {
+		if (ip.getResource(file.value) instanceof ReadableByteChannel bc) {
+			ByteBuffer buf = ByteBuffer.wrap(data, (int)ofs, (int)len);
+			try {
+				while(buf.hasRemaining())
+					if (bc.read(buf) < 0)
+						throw new EOFException("EOF");
+			} catch(IOException e) {
+				try {
+					ip.removeResource(file.value).close();
+				} catch(IOException e1) {e1.printStackTrace();}
+				return new Value(FILE, NO_ELEM, e.getMessage().getBytes(UTF_8), -1);
+			}
+		}
+		return file;
+	}
+
+	@Impl(inputs = 4, useIp = true)
+	public static Value fileWrite(Interpreter ip, Value file, byte[] data, long ofs, long len) {
+		if (ip.getResource(file.value) instanceof WritableByteChannel bc) {
+			ByteBuffer buf = ByteBuffer.wrap(data, (int)ofs, (int)len);
+			try {
+				while(buf.hasRemaining())
+					if (bc.write(buf) < 0)
+						throw new EOFException("EOF");
+			} catch(IOException e) {
+				try {
+					ip.removeResource(file.value).close();
+				} catch(IOException e1) {e1.printStackTrace();}
+				return new Value(FILE, NO_ELEM, e.getMessage().getBytes(UTF_8), -1);
+			}
+		}
+		return file;
+	}
+
+	@Impl(inputs = 1, useIp = true, outType = "INT")
+	public static long fileLen(Interpreter ip, long file) {
+		if (ip.getResource(file) instanceof SeekableByteChannel sbc)
+			try {
+				return sbc.size() - sbc.position();
+			} catch(IOException e) {
+				return -1;
+			}
+		return 0;
+	}
+
+	@Impl(inputs = 1, useIp = true, outType = "INT")
+	public static long fileClose(Interpreter ip, long file) {
+		Closeable c = ip.removeResource(file);
+		if (c != null) try {
+			c.close();
+			return 0;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return -1;
+	}
+
 
 	@Impl(inputs = 2, outType = "VOID")
 	public static long swt(long path, long count) {
