9be29bf039ebbba739f21017968d26380084ca69
CD4017BE <cd4017be@gmail.com>
2023-05-10 17:47:15 +0200

    convert Circuit Editor to new gui system

---
+++ src/java/cd4017be/dfc/editor/circuit/CircuitObject.java
@@ -0,0 +1,17 @@
+package cd4017be.dfc.editor.circuit;
+
+/**
+ * @author CD4017BE */
+public interface CircuitObject {
+
+	boolean inRange(int x0, int y0, int x1, int y1);
+	short x();
+	short y();
+	CircuitObject pickup();
+	CircuitObject place();
+	CircuitObject pos(int x, int y);
+	void add(CircuitBoard cb);
+	void remove();
+	void draw();
+
+}
